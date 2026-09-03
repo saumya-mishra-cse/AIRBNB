7275879599543f9f6971817cfa64cb440f5cd6ae
Saumya-Mishra <saumyamishra.cs@gmail.com>
2026-09-03 22:13:09 +0530

    Updated and added styling and decoration to the website

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js")
 const path=require("path")
 const methodOverride = require("method-override")
+const ejsmate = require("ejs-mate")
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 async function main(){
@@ -14,6 +15,8 @@ app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
+app.engine("ejs",ejsmate)
+app.use(express.static(path.join(__dirname,"/public")))
 
 main()
   .then(()=>{
@@ -49,6 +52,7 @@ app.get("/listings",async(req,res)=>{
 //New Route
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
+    
 })
 
 //Show route
@@ -92,4 +96,4 @@ app.listen("8080",()=>{
     console.log("server is listening to port 8080")
 })
 
-
+                
